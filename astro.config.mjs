35de2ee6c4767352beb2e1643c626ec6e33e6a94
hupeiwen <hpw1112@163.com>
2025-03-21 11:50:49 +0800

    i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
-    
+    i18n: {
+        locales: ["en", "zh-cn"],
+        defaultLocale: "zh-cn",
+    }
 });
