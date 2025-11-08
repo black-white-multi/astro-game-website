b3cd4324defa095d7c22226ba55b7aa43d80a781
BlackWhite <sprite2345@gmail.com>
2025-11-08 15:50:34 +0800

    site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,10 @@ export default defineConfig({
     i18n: {
         locales: ["en", "zh-cn"],
         defaultLocale: "zh-cn",
+    },
+    site: 'https://rich-server.oss-cn-hongkong.aliyuncs.com',
+    outDir: './dist',
+    build: {
+        format: 'directory'
     }
 });
